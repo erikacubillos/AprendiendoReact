e8dfa0f55476c0260d5493f5d01027aa8c3cb121
ekajohana <ekajohana@gmail.com>
2022-09-27 12:12:35 -0500

    clase 62

--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -2,15 +2,17 @@
 //propias del sistema
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';//formulario reactivos
 //2. las importaciones de terceros
 
-//2. las importaciones de creadas
+//2. las importaciones creadas
 import { MainpageComponent } from './mainpage/mainpage.component';
+import { PersonajesComponent } from './personajes/personajes.component';
 
 @NgModule({
   declarations: [
-    MainpageComponent
+    MainpageComponent,
+    PersonajesComponent
   ],
   exports: [
     MainpageComponent
@@ -18,6 +20,6 @@ import { MainpageComponent } from './mainpage/mainpage.component';
   imports: [
     CommonModule,
     FormsModule
-  ]
+  ] 
 })
 export class DbzModule { }
--- src/app/dbz/mainpage/mainpage.component.ts
+++ src/app/dbz/mainpage/mainpage.component.ts
@@ -1,28 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interfaces';
 
-
-interface Personaje {
-  nombre: string;
-  poder: number;
-}
 
 @Component({
   selector: 'app-mainpage',
   templateUrl: './mainpage.component.html',
 })
-export class MainpageComponent {
-  
-   
+export class MainpageComponent {  
+
+  personajes:Personaje[] = [
+    {
+      nombre: 'Goku',
+      poder: 100000
+    },
+    {
+      nombre: 'Krilin',
+      poder: 100
+    },
+    {
+      nombre: 'vegueta',
+      poder: 5000
+    }
+  ]
   //metodos
   nuevo: Personaje= {
     nombre: '',
     poder: 0
   }
   agregar(){
-    if( this.nuevo.nombre.trim().length === 0){
-      return;
-    }
+    //validación por si no hay
+    if(this.nuevo.nombre.trim().length === 0){ return; }
     console.log(this.nuevo);
+    this.personajes.push(this.nuevo);
+    this.nuevo = {
+      nombre:"",
+      poder:0
+    };
   }
-
 }
